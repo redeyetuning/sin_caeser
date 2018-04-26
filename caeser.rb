bb8d0d5c5b0fe87b132c10ae9b3691d8264dfc79
Samuel Masters <redeyetuning@gmail.com>
2018-04-26 12:59:07 +0400

    Move caser_cypher to ext file, add style.

--- caeser.rb
+++ caeser.rb
@@ -1,52 +1,12 @@
+require './lib/caeser_cypher.rb'
 require 'sinatra' 
 require  'sinatra/reloader' if development?
 
-
-
-
-def pop_shift_select
+def pop_shift_select # Create shift factor options for 'shift_in' dropdown
 	 (1..25).to_a.map {|x|"<option value = '#{x}'>#{x}</option>"}.join
 end
 
-def caeser_cipher string, shift= 0
-		 
-	output = []
-
-	string.split("").each do |char|
-		i = char.ord
-		if i >= 65 && i <= 90
-			if i + shift > 90
-				output << (char.ord + shift - 26).chr
-			elsif i + shift < 65
-				output << (char.ord + shift + 26).chr
-			else
-				output << (char.ord + shift).chr
-			end
-
-		elsif i >= 97 && i <= 122
-			if i + shift > 122
-				output << (char.ord + shift - 26).chr
-			elsif i + shift < 97
-				output << (char.ord + shift + 26).chr
-			else
-				output << (char.ord + shift).chr
-			end
-		else
-			output << char
-
-		end
-	end
-
-	output.join
-
-end
-
-
-
-
-shift_val = pop_shift_select
-
-
+shift_val = pop_shift_select 
 
 get '/' do
 	output = caeser_cipher(params["text_in"], params["shift_in"].to_i) if params["text_in"]
---
+++ lib/caeser_cypher.rb
@@ -0,0 +1,33 @@
+
+def caeser_cipher string, shift= 0
+		 
+	output = []
+
+	string.split("").each do |char|
+		i = char.ord
+		if i >= 65 && i <= 90
+			if i + shift > 90
+				output << (char.ord + shift - 26).chr
+			elsif i + shift < 65
+				output << (char.ord + shift + 26).chr
+			else
+				output << (char.ord + shift).chr
+			end
+
+		elsif i >= 97 && i <= 122
+			if i + shift > 122
+				output << (char.ord + shift - 26).chr
+			elsif i + shift < 97
+				output << (char.ord + shift + 26).chr
+			else
+				output << (char.ord + shift).chr
+			end
+		else
+			output << char
+
+		end
+	end
+
+	output.join
+
+end
